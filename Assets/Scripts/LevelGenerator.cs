eef5caf2d85eaf72bd647fe2732970ca034266af
Malcjo <joshuaeric.malcolm@gmail.com>
2024-09-28 23:35:18 +1200

    added placeholder art packs

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -19,7 +19,7 @@ public class LevelGenerator : MonoBehaviour
     public void startSpawning()
     {
         startRandomSpawn = true;
-        nextSpawnX = player.position.x + (platformLength /2);  // Set spawn position after the custom section
+        nextSpawnX = player.position.x + (platformLength / 2);  // Set spawn position after the custom section
     }
     void Update()
     {
